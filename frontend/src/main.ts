3ec0f9727d50be303dda9d32ca4e89fbff3b73fb
hisho-123 <vahcp92774@yahoo.co.jp>
2024-11-04 17:00:44 +0900

    構文チェック・修正を自動で可能にした eslint prettier

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,29 +1,28 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './style.css'
-import router from './router'
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-import '@mdi/font/css/materialdesignicons.css'
-import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./style.css";
+import router from "./router";
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import "@mdi/font/css/materialdesignicons.css";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
 
-
-const app = createApp(App)
+const app = createApp(App);
 
 const vuetify = createVuetify({
   components,
   directives,
   icons: {
-    defaultSet: 'mdi',
+    defaultSet: "mdi",
     aliases,
     sets: {
-      mdi
-    }
-  }
-})
+      mdi,
+    },
+  },
+});
 
-app.use(router)
-app.mount('#app')
-app.use(vuetify)
+app.use(router);
+app.mount("#app");
+app.use(vuetify);
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,23 +1,23 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import Test from '../pages/Test.vue'
-import Testest from '../pages/Testest.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import Test from "../pages/Test.vue";
+import Testest from "../pages/Testest.vue";
 
 const routes = [
   {
-    path: '/test',
-    name: 'Test',
-    component: Test
+    path: "/test",
+    name: "Test",
+    component: Test,
   },
   {
-    path: '/testest',
-    name: 'Testest',
-    component: Testest
-  }
-]
- 
+    path: "/testest",
+    name: "Testest",
+    component: Testest,
+  },
+];
+
 const router = createRouter({
   history: createWebHistory(),
   routes,
-})
- 
+});
+
 export default router;
