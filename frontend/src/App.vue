4d56c8c9d263475a846a50f3807a57475c4f5ee7
hisho-123 <vahcp92774@yahoo.co.jp>
2024-11-07 22:05:41 +0900

    サイドバーを作成(issue/15)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,3 +1,29 @@
+<script setup lang="ts">
+import Sidebar from "./navigation/Sidebar.vue";
+</script>
 <template>
-  <router-view />
+  <div class="display">
+    <div class="sidebar">
+      <Sidebar />
+    </div>
+    <div class="body">
+      <router-view />
+    </div>
+  </div>
 </template>
+<style lang="scss">
+.display {
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  padding: 0 !important;
+}
+.sidebar {
+  background-color: aliceblue;
+  height: 100%;
+  width: 200px;
+}
+.body {
+  width: calc(100% - 200px);
+}
+</style>
