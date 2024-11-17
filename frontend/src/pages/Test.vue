531c103df7743bd347f0f867ece227a2a9a3a2ae
hisho-123 <vahcp92774@yahoo.co.jp>
2024-11-17 14:55:29 +0900

    ボタンコンポーネントを作成 use(vuetify)を移動 scssのライブラリをインストール

--- frontend/src/pages/Test.vue
+++ frontend/src/pages/Test.vue
@@ -1,12 +1,21 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
-console.log("Test");
-console.error("test");
+import Button from '../components/button/index.vue'
 
-const count = ref(0);
+const count = ref(0)
+console.error('count: '+count.value)
 </script>
 <template>
   <div>test 成功</div>
-  <v-btn @click="count++">{{ count }} </v-btn>
+  <div>
+    <Button
+      color="primary"
+      :content="'count: '+count"
+      size="small"
+      :cancelBtn="true"
+      position="right"
+      @click="count++"
+    ></Button>
+  </div>
 </template>
