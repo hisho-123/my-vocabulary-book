7a7ce03bf84f1bb3ca587b11349c87d10ee44fec
hisho-123 <vahcp92774@yahoo.co.jp>
2024-11-17 15:53:07 +0900

    dialog の修正 ディレクトリ名の最初を大文字に統一 使用側でのimport pathを絶対パスに修正(#HEAD)

--- frontend/src/pages/Test.vue
+++ frontend/src/pages/Test.vue
@@ -2,10 +2,12 @@
 import { ref } from "vue";
 import { test } from "@/term/page.ts"
 
-import Button from '../components/Button/index.vue'
+import Button from '@/components/Button/index.vue'
+import Dialog from '@/components/Dialog/index.vue'
 
 const count = ref(0)
-console.error('count: '+count.value)
+const dialog = ref(false)
+
 </script>
 <template>
   <div>{{ test.test }}</div>
@@ -15,8 +17,19 @@ console.error('count: '+count.value)
       :content="'count: '+count"
       size="small"
       :secondBtn="true"
-      :disabled="true"
-      @click="count++"
+      secondBtnContent="count=0"
+      @firstClick="dialog = !dialog"
+      @secondClick="count=0"
     ></Button>
   </div>
+  <Dialog
+    :isDialog="dialog"
+    title="title"
+    content="contentcontent"
+    btnContent="count+1"
+    secondBtnContent="cancel"
+    :secondBtn="true"
+    @firstClick="count++"
+    @secondClick="dialog = !dialog"
+  />
 </template>
--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,17 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Test from "@/pages/Test.vue";
-import Testest from "@/pages/Testest.vue";
 
 const routes = [
   {
     path: "/test",
     name: "Test",
     component: Test,
-  },
-  {
-    path: "/testest",
-    name: "Testest",
-    component: Testest,
   },
 ];
 
