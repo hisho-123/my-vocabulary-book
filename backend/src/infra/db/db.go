42b35450a6581f2a29bf1015aafe4ca9eb7e6fd6
hisho.suzuki <hisho.suzuki@optim.co.jp>
2025-01-02 15:56:01 +0900

    mysqlへの接続確認

---
+++ backend/src/infra/db/db.go
@@ -0,0 +1,18 @@
+package db
+
+import (
+	"database/sql"
+	"log"
+)
+
+func NewDB() *sql.DB {
+	db, err := sql.Open("mysql", "root:root@tcp(127.17.0.1:3306)/my_vocabulary_book")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+	return db
+}
+
