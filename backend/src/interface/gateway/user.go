30e8b93b412113d6c428f57d52269498c307166a
hisho.suzuki <hisho.suzuki@optim.co.jp>
2025-01-13 12:20:54 +0900

    user情報のDB操作機能を追加(issue/28) userの作成 userの取得 userの削除

---
+++ backend/src/interface/gateway/user.go
@@ -0,0 +1,64 @@
+package gateway
+
+import (
+	"backend/src/infra/db"
+	"fmt"
+	"log"
+)
+
+// user作成
+func CreateUserToDB(userName string, hashedPassword string) error {
+	db := db.OpenDB()
+	defer db.Close()
+
+	queryCreateUser := "insert into users (user_name, password) values (?, ?);"
+
+	_, err := db.Exec(queryCreateUser, userName, hashedPassword)
+	if err != nil {
+		log.Fatal(err)
+		return fmt.Errorf("failed to create user")
+	}
+
+	return nil
+}
+
+// user取得
+func GetUser(userName string) (userId int, hashedPassword string, err error) {
+	db := db.OpenDB()
+	defer db.Close()
+	
+	queryGetUser := "select user_id, password from users where user_name = ?;"
+	userRows := db.QueryRow(queryGetUser, userName)
+
+	if err := userRows.Scan(&userId, &hashedPassword); err != nil {
+		if err != nil {
+			log.Fatal(err)
+			return 0, "", fmt.Errorf("failed to create user")
+		}
+	}
+
+	return userId, hashedPassword, nil
+}
+
+func DeleteUserByUserId(userId int) error {
+	db := db.OpenDB()
+	defer db.Close()
+
+	queryDeleteUser := "delete from users where user_id = ?;"
+	res, err := db.Exec(queryDeleteUser, userId)
+	if err != nil {
+		log.Fatal(err)
+		return fmt.Errorf("failed to delete user")
+	}
+	rowsAffected, err := res.RowsAffected()
+	if err != nil {
+		log.Fatal(err)
+		return fmt.Errorf("failed to check rows affected")
+	}
+	if rowsAffected == 0 {
+		log.Printf("no user found with user_id %d", userId)
+		return fmt.Errorf("no user found with user_id %d", userId)
+	}
+
+	return nil
+}
