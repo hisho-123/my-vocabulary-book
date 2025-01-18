124d825fdebdf7729c7840bbd93e121d9a594bee
hisho.suzuki <hisho.suzuki@optim.co.jp>
2025-01-18 18:04:58 +0900

    認識の齟齬など修正(issue/28)

--- backend/src/interface/gateway/create.go
+++ backend/src/interface/gateway/create.go
@@ -3,12 +3,13 @@ package gateway
 import (
 	"backend/src/domain"
 	"backend/src/infra/db"
+	"fmt"
 	"log"
 	"strconv"
 )
 
 // 単語帳の作成
-func CreateBookByUserId(userId int, bookName string, words []domain.Word) {
+func CreateBookByUserId(userId int, bookName string, words []domain.Word) error {
 	db := db.OpenDB()
 	defer db.Close()
 
@@ -17,6 +18,7 @@ func CreateBookByUserId(userId int, bookName string, words []domain.Word) {
 	_, err := db.Exec(queryCreateBook, strconv.Itoa(userId), bookName)
 	if err != nil {
 		log.Fatal(err)
+		return fmt.Errorf("failed to create book")
 	}
 
 	var bookId string
@@ -24,25 +26,32 @@ func CreateBookByUserId(userId int, bookName string, words []domain.Word) {
 	bookIdRow := db.QueryRow(queryGetBookId, bookName)
 	if err := bookIdRow.Scan(&bookId); err != nil {
 		log.Fatal(err)
+		return fmt.Errorf("failed to get book_id")
 	}
 
 	queryCreateWord := "insert into words (book_id, word, translated_word) values (?, ?, ?)"
 	for _, v := range words {
 		_, err := db.Query(queryCreateWord, bookId, v.Word, v.Translated)
 		if err != nil {
+			queryDeleteWords := "delete from words where book_id = ?;"
+			db.Query(queryDeleteWords, bookId)
+
 			log.Fatal(err)
+			return fmt.Errorf("failed to create words")
 		}
 	}
+	return nil
 }
 
 // 既存の単語帳に単語を登録
-func CreateWordByBookId(bookId int, word string, translated string) {
+func CreateWordByBookId(bookId int, word string, translated string) error {
 	db := db.OpenDB()
 	defer db.Close()
 
 	queryCreateWord := "insert into words (book_id, word, translated_word) values (?, ?, ?)"
 	_, err := db.Query(queryCreateWord, bookId, word, translated)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Errorf("failed to create word")
 	}
+	return nil
 }
--- backend/src/interface/gateway/read.go
+++ backend/src/interface/gateway/read.go
@@ -3,64 +3,75 @@ package gateway
 import (
 	"backend/src/domain"
 	"backend/src/infra/db"
+	"database/sql"
+	"errors"
+	"fmt"
 	"log"
 	"strconv"
-
-	// "time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
-func GetBookListByUserId(userId int) []domain.Book {
+func GetBookListByUserId(userId int) (books []domain.Book, err error) {
 	db := db.OpenDB()
 	defer db.Close()
 
-	queryGetBookList := "select book_id, book_name from books where user_id = ?"
+	queryGetBookList := "select book_id, book_name, first_review from books where user_id = ?"
 
-	var books []domain.Book
 	var bookId int
 	var bookName string
-	// var RecentReview time.Time
+	var firstReview string
 
 	rows, err := db.Query(queryGetBookList, strconv.Itoa(userId))
 	if err != nil {
 		log.Fatal(err)
+		return nil, fmt.Errorf("failed to query get books")
 	}
 
 	for rows.Next() {
-		if err := rows.Scan(&bookId, &bookName); err != nil {
+		if err := rows.Scan(&bookId, &bookName, &firstReview); err != nil {
 			log.Fatal(err)
+			return nil, fmt.Errorf("failed to scan book")
 		}
 
 		books = append(books, domain.Book{
 			Id:     &bookId,
 			UserId: userId,
 			Name:   bookName,
-			// RecentReview: RecentReview,
+			FirstReview: &firstReview,
 		})
-
 	}
 
-	return books
+	if err := rows.Err(); err != nil {
+		log.Fatal(err)
+		return nil, fmt.Errorf("rows iteration error")
+	}
+
+	return books, nil
 }
 
-func GetBookByBookId(bookId int) (bookName string, words []domain.Word) {
+func GetBookByBookId(bookId int) (bookName string, words []domain.Word, err error) {
 	db := db.OpenDB()
 	defer db.Close()
 
 	// 単語帳の名前を取得
 	queryGetBook := "select book_name from books where book_id = ?"
-	bookNameRow := db.QueryRow(queryGetBook, bookId)
-
-	if err := bookNameRow.Scan(&bookName); err != nil {
+	err = db.QueryRow(queryGetBook, bookId).Scan(&bookName)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			log.Fatal(err)
+			return "", nil, fmt.Errorf("word not found")
+		}
 		log.Fatal(err)
+		return "", nil, fmt.Errorf("failed to get words")
 	}
 
 	// 単語取得
-	queryReaWords := "select word_id, word, translated_word from words where book_id = ?"
-	wordsRows, err := db.Query(queryReaWords, bookId)
+	queryRowWords := "select word_id, word, translated_word from words where book_id = ?"
+	wordsRows, err := db.Query(queryRowWords, bookId)
 	if err != nil {
 		log.Fatal(err)
+		return "", nil, fmt.Errorf("failed to query get words")
 	}
 
 	var wordId int
@@ -70,6 +81,7 @@ func GetBookByBookId(bookId int) (bookName string, words []domain.Word) {
 	for wordsRows.Next() {
 		if err := wordsRows.Scan(&wordId, &word, &translated); err != nil {
 			log.Fatal(err)
+			return "", nil, fmt.Errorf("failed to scan word")
 		}
 
 		words = append(words, domain.Word{
@@ -80,5 +92,10 @@ func GetBookByBookId(bookId int) (bookName string, words []domain.Word) {
 		})
 	}
 
-	return bookName, words
+	if err := wordsRows.Err(); err != nil {
+		log.Fatal(err)
+		return "", nil, fmt.Errorf("wordsRows iteration error")
+	}
+
+	return bookName, words, nil
 }
